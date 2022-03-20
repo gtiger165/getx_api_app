4e336e23017c029ae5fabb2787c370f30f9a48f0
hirarki wendri <ventus.kiki@gmail.com>
2022-03-20 22:35:30 +0700

    refactor: add default content  type attribute, and response modifier method.

--- lib/core/extensions/base_provider.dart
+++ lib/core/extensions/base_provider.dart
@@ -1,10 +1,18 @@
 import 'package:get/get_connect.dart';
+import 'package:getx_api_app/core/utils/log_util.dart';
 
 class BaseProvider extends GetConnect {
   @override
   void onInit() {
     super.onInit();
     httpClient.baseUrl = "https://makeup-api.herokuapp.com/api/v1/";
+    httpClient.defaultContentType = "application/json";
+
+    // * required must return response
+    httpClient.addResponseModifier((request, response) {
+      LogUtil().errors("${response.bodyString}");
+      return response;
+    });
   }
 
   Future<Response<T>> getWithEndpoint<T>(String endpoint) => get(endpoint);
